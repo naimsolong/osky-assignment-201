a67f7a4b20b7ba42cebe608e7a74844d7d715466
Amirul <amirul.m@xeersoft.com>
2021-07-05 17:46:01 +0800

    Update some env function

--- autoload.php
+++ autoload.php
@@ -4,3 +4,16 @@ require __DIR__ . '/vendor/autoload.php';
 
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
 $dotenv->load();
+
+if(!function_exists('env')) {
+	function env($key, $default = null)
+	{
+		$value = $_ENV[$key];
+
+		if ($value === false) {
+			return $default;
+		}
+
+		return $value;
+	}
+}
